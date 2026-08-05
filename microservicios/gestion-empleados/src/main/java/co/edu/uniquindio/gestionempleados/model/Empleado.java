443b207deb8065d654959438aa63049d8421d366
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2026-08-05 12:21:37 -0500

    feat: estructura inicial y Dockerfile para gestion-empleados

---
+++ microservicios/gestion-empleados/src/main/java/co/edu/uniquindio/gestionempleados/model/Empleado.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.gestionempleados.model;
+
+public class Empleado {
+}
---
+++ microservicios/gestion-empleados/src/main/java/co/edu/uniquindio/gestionempleados/repository/EmpleadoRepository.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.gestionempleados.repository;
+
+public class EmpleadoRepository {
+}
